8102218bc8b679bb2258404393008f1a42ad8861
DaniCcimo <bessmertnyj228@gmail.com>
2023-11-21 20:37:07 +0200

    Finished homework_22

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let arrLength = +prompt(`Введите длину массива:`);
+let newArr = [];
+
+if (!isNaN(arrLength) && arrLength > 0) {
+    newArr = new Array(arrLength);
+} else {
+    console.log(`Должно быть положительное число(Больше нуля)`);
+}
+
+for (let i = 0; i < arrLength; i++) {
+    let arrItem = prompt(`Введите элемент массива:`);
+    newArr[i] = arrItem;
+}
+
+console.log(newArr);
