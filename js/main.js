44c360a3b7dbf2183fb883c068232d37482625ab
DaniCcimo <bessmertnyj228@gmail.com>
2023-11-22 12:48:07 +0200

    Change quotes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-let arrLength = +prompt(`Введите длину массива:`);
+let arrLength = +prompt('Введите длину массива:');
 let newArr = [];
 
 if (!isNaN(arrLength) && arrLength > 0) {
     newArr = new Array(arrLength);
 } else {
-    console.log(`Должно быть положительное число(Больше нуля)`);
+    console.log('Должно быть положительное число(Больше нуля)');
 }
 
 for (let i = 0; i < arrLength; i++) {
-    let arrItem = prompt(`Введите элемент массива:`);
+    let arrItem = prompt('Введите элемент массива:');
     newArr[i] = arrItem;
 }
 
