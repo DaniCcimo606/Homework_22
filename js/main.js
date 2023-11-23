61446e94afc0bd7bdb757961a733f8c37bb04588
DaniCcimo <bessmertnyj228@gmail.com>
2023-11-23 17:40:07 +0200

    Change code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
+// let arrLength = +prompt('Введите длину массива:');
+// let newArr = [];
+
+// if (!isNaN(arrLength) && arrLength > 0) {
+//     newArr = new Array(arrLength);
+// } else {
+//     console.log('Должно быть положительное число(Больше нуля)');
+// }
+
+// for (let i = 0; i < arrLength; i++) {
+//     let arrItem = prompt('Введите элемент массива:');
+//     newArr[i] = arrItem;
+// }
+
+// console.log(newArr);
+
+
 let arrLength = +prompt('Введите длину массива:');
 let newArr = [];
 
 if (!isNaN(arrLength) && arrLength > 0) {
-    newArr = new Array(arrLength);
+    for (let i = 0; i < arrLength; i++) {
+        let arrItem = prompt('Введите элемент массива:');
+        newArr[i] = arrItem;
+    }
 } else {
     console.log('Должно быть положительное число(Больше нуля)');
 }
 
-for (let i = 0; i < arrLength; i++) {
-    let arrItem = prompt('Введите элемент массива:');
-    newArr[i] = arrItem;
-}
 
 console.log(newArr);
